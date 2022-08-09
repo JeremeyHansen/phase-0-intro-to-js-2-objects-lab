f4a926ea97c31a24b8330132ac62dac96fe3eeba
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-08-09 11:18:55 -0400

    reviewing another lesson on objects and how to modify in multiple ways

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    employee.streetAddress = "12 Broadway";
+
+    return employee;
+}
+function deleteFromEmployeeByKey(obj, key, value){
+    const newEmployee = {...obj};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+delete newEmployee.name 
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value){
+    delete employee.name
+    return employee;
+}
